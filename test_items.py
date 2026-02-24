729e525e13174db81a2a9cd76ce7ca13a9612d20
RikkaFurude <94025617+RikkaFurude@users.noreply.github.com>
2026-02-24 20:58:56 +0500

    Update test_items.py

--- test_items.py
+++ test_items.py
@@ -2,15 +2,14 @@ import time
 from selenium.webdriver.common.by import By
 
 def test_guest_should_see_add_to_basket_button(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+    link = "http://selenium1py.pythonanywhere.com"
     browser.get(link)
     
-    # Оставляем браузер открытым на 30 секунд для визуальной проверки языка (по условию задания)
-    # time.sleep(30)
+    # Даем время рецензенту визуально проверить язык кнопки
+    time.sleep(30)
     
-    # Проверяем наличие кнопки добавления в корзину
-    # Используем селектор, который однозначно находит кнопку
-    add_to_basket_buttons = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    # Ищем кнопку по селектору
+    buttons = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     
-    # Проверяем, что список кнопок не пуст
-    assert len(add_to_basket_buttons) > 0, "Button 'Add to basket' not found on the page!"
+    # Проверяем, что кнопка нашлась
+    assert len(buttons) > 0, "Кнопка добавления в корзину не найдена!"
